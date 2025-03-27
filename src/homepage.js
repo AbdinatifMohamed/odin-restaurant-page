5f5a475a9071a0a05712ee44ece258cd51234f21
Abdinatif Mohamed <197330086+AbdinatifMohamed@users.noreply.github.com>
2025-03-26 19:09:46 -0500

    finished the html/css part of(homepage, menu) and now starting on the contact one.

---
+++ src/homepage.js
@@ -0,0 +1,11 @@
+/*
+
+  <div class="homepage">
+            <h2>MOBILE TACO VAN</h2>
+            <h2>WHERE EVERY TACO FEELS LIKE HOME</h2>
+            <h2>WELCOME TO MOBILE TACO VAN</h2>
+            <button class="homepage__btn btn">EXPLORE</button>
+    </div>
+
+
+*/
---
+++ src/menu.js
@@ -0,0 +1,29 @@
+/*
+  <div class="menu">
+            <div class="menu__foods">
+                <h3>TACOS & MORE</h3>
+                <div class="foods__item">
+                    <div class="item">
+                        <img src="./assets/imgs/foods-drinks/tacos.webp" alt="">
+                        <p>TACOS</p>
+                        <p>$2</p>
+                    </div>
+                </div>
+            </div>
+            <div class="menu__drinks">
+                <h3>SODAS & JUGOS</h3>
+                <div class="drinks__item">
+                    <div class="item">
+                        <img src="./assets/imgs/foods-drinks/tacos.webp" alt="">
+                        <p>TACOS</p>
+                        <p>$2</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+*/
